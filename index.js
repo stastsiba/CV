dee6343de9c972a4c3f99b472fdcd866e631c65e
Stas Tsiba <88132424+stastsiba@users.noreply.github.com>
2022-05-23 17:08:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let arr = [1, 5, 2,66 ,2, 8, 10];
+
+// let newArr = arr.reduce((acc, item) => acc + item, 0)
+// console.log(newArr);
+
+// let newArray = arr.concat(arr)
+// console.log(newArray);
+
+// let max = arr.reduce((acc, item) => Math.max(acc, item))
+// console.log(max)
+
+// console.log(arr.sort((x1, x2) => x1 - x2))
